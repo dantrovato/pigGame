300e388588b1a654a847e4d155b3b015acd17ced
dantrovato <plasticsoup@yahoo.co.uk>
2018-03-15 23:14:49 +0000

    add logic for the double 6 rule. 2 consecutive 6 reset player's score

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer, gamePlaying;
+var scores, roundScore, activePlayer, gamePlaying, sixesCount;
 
 function init() {
     scores = [0,0];
     roundScore = 0,
     activePlayer = 0;
     gamePlaying = true;
+    sixesCount = 0;
     document.querySelector('.dice').style.display = 'none';
 
     document.getElementById('score-0').textContent = '0';
@@ -47,32 +48,45 @@ init();
 document.querySelector('.btn-roll').addEventListener('click', function() {
     if (gamePlaying) {
         // 1. random number
-        var dice = Math.floor(Math.random() * 6) + 1;
+        var dice = Math.floor(Math.random() * 2) + 5;
         // 2. display result
         var diceDOM = document.querySelector('.dice');
         diceDOM.style.display = 'block';
         diceDOM.src = 'dice-' + dice + '.png';
 
         //3. update score if number higher than 1
-        if (dice !== 1) {
-          //add score
-          roundScore += dice;
-          //scores[activePlayer] += dice;
-          document.getElementById('current-' + activePlayer).textContent = roundScore;
-          //document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+        if (dice !== 1 && dice !== 6) {
+            sixesCount = 0;
+            //add score
+            roundScore += dice;
+            document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+            document.getElementById('current-' + activePlayer).textContent = roundScore;
+            console.log('this is a 5');
+        } else if (dice === 6) {
+            if (sixesCount === 0) {
+                sixesCount++;
+                roundScore += dice;
+                document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+                document.getElementById('current-' + activePlayer).textContent = roundScore;
+                console.log('this is the first 6');
+            } else {
+              sixesCount++;
+              scores[activePlayer] = 0;
+              roundScore = 0;
+              document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+              document.getElementById('current-' + activePlayer).textContent = roundScore;
 
-        } else {
-          // continue
-          nextPlayer();
-          //document.getElementById('current-1' + activePlayer).textContent = 0;
+              nextPlayer();
+              console.log('this is the second 6');
+              sixesCount = 0;
+            }
         }
+        console.log('sixesCount: ' + sixesCount);
     }
-
-
-
 });
 document.querySelector('.btn-hold').addEventListener('click', function() {
     if (gamePlaying) {
+      sixesCount = 0;
       // add score to global score
       scores[activePlayer] += roundScore;
       // add score to top window
@@ -81,7 +95,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
       document.getElementById('current-' + activePlayer).textContent = 0;
 
       // check if player has hit 100
-      if (scores[activePlayer] >= 20) {
+      if (scores[activePlayer] >= 100) {
 
         document.getElementById('name-' + activePlayer).textContent = 'you win';
         document.querySelector('.dice').style.display = 'none';
@@ -108,7 +122,7 @@ document.querySelector('.btn-new').addEventListener('click', init);
         if (dice === 6) {
               -RESET TOTAL SCORE-
         } else
-  } else play game 
+  } else play game
 
  */
 
